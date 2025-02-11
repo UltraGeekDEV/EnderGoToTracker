75d094f582008b6eeea706edcef0b7beb7e89458
Attila <hunkrekker@gmail.com>
2025-02-11 13:45:57 +0100

    Implemented Quaternion

--- src/Quaternion/Quaternion.h
+++ src/Quaternion/Quaternion.h
@@ -8,6 +8,6 @@ static Quaternion Euler(double x,double y,double z);
 Quaternion(double _w,double _x,double _y,double _z):w(_w),x(_x),y(_y),z(_z){}
 Quaternion():w(1),x(0),y(0),z(0){}
 Quaternion operator*(const Quaternion& q2);
-Quaternion operator*(const Vector3& q2);
+Vector3 operator*(const Vector3& q2);
 Quaternion Inverse();
 };
--- src/Vector3/Vector3.h
+++ src/Vector3/Vector3.h
@@ -4,4 +4,5 @@ class Vector3{
 public:
 double x,y,z;
 Vector3(double _x,double _y,double _z):x(_x),y(_y),z(_z){}
+Vector3():x(0),y(0),z(0){}
 };
